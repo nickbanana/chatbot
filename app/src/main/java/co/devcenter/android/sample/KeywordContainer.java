f6382fc58afd0d19b9a43d58d2ddfe30244f3bd1
nick.chi <nick.chi@gmail.com>
2016-08-03 02:28:58 +0800

    add Keywords and KeywordContainer

---
+++ app/src/main/java/co/devcenter/android/sample/KeywordContainer.java
@@ -0,0 +1,26 @@
+package co.devcenter.android.sample;
+
+import java.util.ArrayList;
+
+
+public class KeywordContainer {
+
+
+
+    private ArrayList<Keywords> Semantic_Frame;
+    private int Framecount;
+
+    public int getFramecount()
+    {
+        return Framecount;
+    }
+
+    public void setFramecount(int framecount)
+    {
+        Framecount = framecount;
+    }
+
+    Keywords a;
+
+
+}
---
+++ app/src/main/java/co/devcenter/android/sample/Keywords.java
@@ -0,0 +1,15 @@
+package co.devcenter.android.sample;
+
+
+public class Keywords {
+
+    private String keyword;
+    private String property;
+
+    public Keywords(String keyword, String property)
+    {
+        super();
+        this.keyword=keyword;
+        this.property=property;
+    }
+}
